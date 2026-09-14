97c66416fc71af1a3397c1c1436d3f314c427853
Bastian Nazaromi <bastian.nazaromi@gmail.com>
2026-09-14 10:03:22 +0700

    feat: implement user login API and sessions table (#6)

--- src/index.ts
+++ src/index.ts
@@ -2,31 +2,33 @@ import { Elysia } from "elysia";
 import { db } from "./db";
 import { users } from "./db/schema";
 import { usersRoutes } from "./routes/usersRoutes";
+import { authRoutes } from "./routes/authRoutes";
 
 const app = new Elysia()
-  .use(usersRoutes)
-  .get("/", () => ({
-    status: "success",
-    message: "Welcome to ElysiaJS + Bun + Drizzle + MySQL API",
-  }))
-  .get("/users", async ({ set }) => {
-    try {
-      const allUsers = await db.select().from(users);
-      return {
+    .use(usersRoutes)
+    .use(authRoutes)
+    .get("/", () => ({
         status: "success",
-        data: allUsers,
-      };
-    } catch (error: any) {
-      set.status = 500;
-      return {
-        status: "error",
-        message: "Failed to fetch users from database",
-        error: error?.message || String(error),
-      };
-    }
-  })
-  .listen(process.env.PORT || 3000);
+        message: "Welcome to ElysiaJS + Bun + Drizzle + MySQL API",
+    }))
+    .get("/users", async ({ set }) => {
+        try {
+            const allUsers = await db.select().from(users);
+            return {
+                status: "success",
+                data: allUsers,
+            };
+        } catch (error: any) {
+            set.status = 500;
+            return {
+                status: "error",
+                message: "Failed to fetch users from database",
+                error: error?.message || String(error),
+            };
+        }
+    })
+    .listen(process.env.PORT || 3000);
 
 console.log(
-  `🦊 Elysia server is running at ${app.server?.hostname}:${app.server?.port}`
+    `🦊 Elysia server is running at ${app.server?.hostname}:${app.server?.port}`,
 );
